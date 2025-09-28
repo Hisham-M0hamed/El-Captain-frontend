c568cc616f4e8d0196f25446c21fa93339747730
Hisham Mohamed <hisham.0mohamed7@gmail.com>
2025-09-28 14:03:28 -0700

    setup repo file

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,28 +1,47 @@
 import React from "react";
 import { FaArrowDown } from "react-icons/fa";
+import { FaCartShopping } from "react-icons/fa6";
+import { FaCaravan } from "react-icons/fa";
 
+// import "./navbar.css";
 const Navbar = () => {
   return (
-    <div>
-      <div>
-        <h2>El-Captain</h2>
+    <div className="container">
+      <div className="navbar">
+        <div className="logo">
+          <h2>El-Captain</h2>
+          <div>
+            <div>
+              Categories
+              <FaArrowDown style={{ fontSize: "13px", paddingLeft: "2px" }} />
+            </div>
+            <ul className="our-categories">
+              <li>Building materials</li>
+              <li>Chemical industries</li>
+              <li>Pharmaceutical industries</li>
+              <li>Engineering industries</li>
+            </ul>
+          </div>
+        </div>
         <div>
-          Categories
-          <FaArrowDown style={{ fontSize: "13px", paddingLeft: "2px" }} />
+          <nav>
+            <ul>
+              <li>Home</li>
+              <li>Services</li>
+              <li>About US</li>
+              <li>Partners</li>
+              <li>Contact US</li>
+            </ul>
+          </nav>
+        </div>
+        <div className="login">
+          <div className="icons">
+            <FaCartShopping />
+            <FaCaravan />
+          </div>
+          <button>Login</button>
         </div>
       </div>
-      <div>
-        <nav>
-          <ul>
-            <li>Home</li>
-            <li>Services</li>
-            <li>About US</li>
-            <li>Partners</li>
-            <li>Contact US</li>
-          </ul>
-        </nav>
-      </div>
-      <div></div>
     </div>
   );
 };
